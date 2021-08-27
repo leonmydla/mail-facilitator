e59fd990dc04eff1856abb7f807947181b886cb7
Leon Mydla <leonmydla@gmail.com>
2021-08-27 13:01:22 +0200

    MMS-5 add ci / fix test (#3)

--- src/test/kotlin/dev/mydla/mms/mailing/MailingServiceTest.kt
+++ src/test/kotlin/dev/mydla/mms/mailing/MailingServiceTest.kt
@@ -1,15 +1,17 @@
 package dev.mydla.mms.mailing
 
 import io.mockk.every
-import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
-import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.slot
+import io.mockk.spyk
+import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.JavaMailSenderImpl
 import javax.mail.Message
 import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeMessage
@@ -17,36 +19,95 @@ import javax.mail.internet.MimeMessage
 @ExtendWith(MockKExtension::class)
 internal class MailingServiceTest {
 
-    @Suppress("unused")
     @MockK
     private lateinit var config: MailingConfig
 
-    @RelaxedMockK
     private lateinit var jMailSender: JavaMailSender
 
-    @InjectMockKs
     private lateinit var classUnderTest: MailingService
 
+    private val sender = "sender@mail.com"
+    private val recipient = "recipient@mail.com"
+    private val subject = "This is a subject"
+    private val content = "Here is some mail content"
+    private val replyRecipient = "replyRecipient@mail.com"
+
+    @BeforeEach
+    fun setUp() {
+        jMailSender = spyk<JavaMailSenderImpl>()
+        classUnderTest = MailingService(config, jMailSender)
+    }
+
     @Test
-    fun `should assemble mimeMessage correctly`() {
-        val sender = "sender@mail.com"
-        val recipient = "recipient@mail.com"
-        val mail = Mail("subject", "text", "reply@to.com")
-        val mimeMessage = slot<MimeMessage>()
+    fun `should create MimeMessage using the JavaMailSender`() {
+        every { jMailSender.createMimeMessage() } answers { callOriginal() }
+
+        captureMail()
+
+        verify(exactly = 1) { jMailSender.createMimeMessage() }
+    }
+
+    @Test
+    fun `should assign correct sender`() {
+        assertThat(captureMail().from).containsExactly(InternetAddress(sender))
+    }
+
+    @Test
+    fun `should assign correct recipient`() {
+        assertThat(captureMail().getRecipients(Message.RecipientType.TO)).containsExactly(InternetAddress(recipient))
+    }
+
+    @Test
+    fun `should not assign copy recipients`() {
+        val mail = captureMail()
+
+        assertThat(mail.getRecipients(Message.RecipientType.CC)).isNull()
+        assertThat(mail.getRecipients(Message.RecipientType.BCC)).isNull()
+    }
+
+    @Test
+    fun `should assign correct reply recipient`() {
+        assertThat(captureMail().replyTo).containsExactly(InternetAddress(replyRecipient))
+    }
+
+    @Test
+    fun `should assign correct subject`() {
+        assertThat(captureMail().subject).isEqualTo(subject)
+    }
+
+    @Test
+    fun `should assign correct content`() {
+        assertThat(captureMail().content).isEqualTo(content)
+    }
+
+    @Test
+    fun `should send MimeMessage created by the JavaMailSender`() {
+        val expectedMessage = jMailSender.createMimeMessage();
+
+        every { jMailSender.createMimeMessage() } answers { expectedMessage }
+
+        assertThat(captureMail()).isEqualTo(expectedMessage)
+    }
+
+    @Test
+    fun `should send message`() {
+        captureMail()
+
+        verify(exactly = 1) { jMailSender.send(any<MimeMessage>()) }
+    }
+
+    private fun captureMail(): MimeMessage {
+        val message = slot<MimeMessage>()
+        val mail = Mail(subject, content, replyRecipient)
 
         every { config.sender } returns sender
         every { config.recipient } returns recipient
-        every { jMailSender.send(capture(mimeMessage)) } answers { nothing }
+        every { jMailSender.send(capture(message)) } answers { nothing }
 
         classUnderTest.send(mail)
 
-        assertThat(mimeMessage.isCaptured).isTrue()
-        assertThat(mimeMessage.captured.sender).isEqualTo(sender)
-        assertThat(mimeMessage.captured.getRecipients(Message.RecipientType.TO)).isEqualTo(InternetAddress(recipient))
-        assertThat(mimeMessage.captured.getRecipients(Message.RecipientType.CC)).isNull()
-        assertThat(mimeMessage.captured.getRecipients(Message.RecipientType.BCC)).isNull()
-        assertThat(mimeMessage.captured.subject).isEqualTo(mail.subject)
-        assertThat(mimeMessage.captured.content).isEqualTo(mail.text)
-        assertThat(mimeMessage.captured.replyTo).isEqualTo(mail.replyTo)
+        assertThat(message.isCaptured).isTrue()
+
+        return message.captured
     }
 }
