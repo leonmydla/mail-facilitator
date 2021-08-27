3d2949058238c0fa0938192696de06e936cbbdca
Leon Mydla <leonmydla@gmail.com>
2021-08-27 21:17:29 +0200

    MMS-7 refactor configs (#6)

--- src/main/kotlin/dev/mydla/mms/mailing/MailingConfig.kt
+++ src/main/kotlin/dev/mydla/mms/mailing/MailingConfig.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationProperties("mailing")
+@ConfigurationProperties("mms")
 internal class MailingConfig {
 
     lateinit var sender: String
